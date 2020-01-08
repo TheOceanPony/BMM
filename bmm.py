80e3a1a8551f18c476671e98c27d6921a0fe5901
Ocean <55033382+TheOceanPony@users.noreply.github.com>
2020-01-08 23:27:56 +0200

    "get_random_probs" function added

--- bmm.py
+++ bmm.py
@@ -2,6 +2,23 @@ import pandas as pd
 import numpy as np
 from PIL import Image
 
+
+def get_random_probs(ds_size, clusters_amount):
+    rand_matrix = np.random.randint(
+      low=0,
+      high=5000,
+      size=(ds_size, clusters_amount))
+    
+    sum_along_row = np.repeat(
+      rand_matrix.sum(axis=1).reshape((-1, 1)),
+      repeats=2,
+      axis=1)
+    
+    prob_matrix = rand_matrix/sum_along_row
+    return prob_matrix
+  
+  
+
 # Loading the data
 df = pd.read_csv("dataset/ds81.csv")
 N = df.shape[0]
@@ -16,4 +33,7 @@ for i in range(0, N):
   
   
 im = Image.fromarray(np.uint8(p[1] * 255) , 'L')
+im.show()
+
+im = Image.fromarray(np.uint8(p[0] * 255) , 'L')
 im.show()
